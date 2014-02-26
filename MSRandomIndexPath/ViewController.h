d515bba2771871473d0ac0fd61d5113ad1f2716c
murraysagal <murraysagal@mac.com>
2014-02-26 07:38:19 +0000

    Initial Commit

---
+++ MSRandomIndexPath/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MSRandomIndexPath
+//
+//  Created by Murray Sagal on 2/26/2014.
+//  Copyright (c) 2014 Murray Sagal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ MSRandomIndexPath/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MSRandomIndexPath
+//
+//  Created by Murray Sagal on 2/26/2014.
+//  Copyright (c) 2014 Murray Sagal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
